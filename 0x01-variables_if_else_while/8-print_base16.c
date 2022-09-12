f7be90a8ad8d0ab77e54e87581b653f795ba34b9
Kevin <sibusiso.ntuli10@gmail.com>
2022-09-12 15:40:17 -0700

    Hexadeci

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,17 +8,17 @@
 int main(void)
 {
 	char c;
-	
+
 	int d;
-	
+
 	c = 'a';
 	d = 0;
-	while 
+	while
 		(d < 10) {
 			putchar(d + '0');
 			d++;
 		}
-	while 
+	while
 		(c <= 'f') {
 			putchar(c);
 			c++;
